c2a71c3d7eea4cb6a80b9f440c5029babd2e37e7
uralgo2 <uralgo2@gmail.com>
2022-08-26 16:55:10 +0500

    adding payment tinkoff api

--- src/controllers/payments.controller.ts
+++ src/controllers/payments.controller.ts
@@ -9,10 +9,16 @@ import {
 import { PaymentsService, TinkoffStatus } from '../services/payments.service'
 import { ISession, UserRoleEnum } from '../utils.types'
 import { Level } from '../decorators'
+import { TelegramBotService } from '../services/telegram.bot.service'
+import { UsersService } from '../services/users.service'
 
 @Controller('/api/payment')
 export class PaymentsController {
-    constructor(private readonly paymentsService: PaymentsService) {}
+    constructor(
+        private readonly paymentsService: PaymentsService,
+        private readonly telegramBotService: TelegramBotService,
+        private readonly usersService: UsersService,
+    ) {}
 
     @Post('notification')
     @HttpCode(HttpStatus.OK)
@@ -20,6 +26,33 @@ export class PaymentsController {
         @Body('OrderId') orderId: string,
         @Body('Status') status: TinkoffStatus,
     ) {
+        if (status == TinkoffStatus.CONFIRMED) {
+            const order = await this.paymentsService.GetOrder(orderId)
+            const amount = order.amount / 100
+            await this.usersService.PatchUser(order.user, {
+                $inc: {
+                    balance: amount,
+                },
+            })
+
+            await this.telegramBotService.NotifyUser(
+                order.user,
+                `Успешное пополнение на ${amount} руб`,
+            )
+        } else if (
+            status == TinkoffStatus.REJECTED ||
+            status == TinkoffStatus.AUTH_FAIL ||
+            status == TinkoffStatus.CANCELED
+        ) {
+            const order = await this.paymentsService.GetOrder(orderId)
+
+            await this.telegramBotService.NotifyUser(
+                order.user,
+                `Неуспешное пополнение на ${
+                    order.amount / 100
+                } руб. Статус: ${status}`,
+            )
+        }
         console.debug(status)
         console.debug(await this.paymentsService.GetOrder(orderId))
     }
